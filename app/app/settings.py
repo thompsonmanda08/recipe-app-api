6735e92d015d76973e3bd344064e0f9c1af87f0c
Thompson Manda <thompsonmanda08@outlook.com>
2024-05-06 12:15:55 +0200

    move user test api file

--- app/app/settings.py
+++ app/app/settings.py
@@ -140,8 +140,4 @@ AUTH_USER_MODEL = "core.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    # "DEFAULT_AUTHENTICATION_CLASSES": [
-    #     "rest_framework.authentication.TokenAuthentication",
-    #     "rest_framework.authentication.SessionAuthentication",
-    # ]
 }
