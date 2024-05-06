41e5365975bc0273f577983b366b2473e1a011fa
Thompson Manda <thompsonmanda08@outlook.com>
2024-05-06 16:02:38 +0200

    hot fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,8 +81,6 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         "ENGINE": "django.db.backends.postgresql",
         "PORT": os.environ.get("DB_PORT"),
         "HOST": os.environ.get("DB_HOST"),
