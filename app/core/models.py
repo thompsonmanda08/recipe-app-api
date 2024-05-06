8040dcb62feb2c8891fd7b2437c54957b4febcfa
Thompson Manda <thompsonmanda08@outlook.com>
2024-05-06 15:47:41 +0200

    Add Auth Code

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
+"""DATABASE MODELS"""
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, Save and Return new User"""
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        # if not password:
+        #     raise ValueError("Users must have a password")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, Save and Return new Superuser"""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
